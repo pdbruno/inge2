ad9641c8993d25ed24044b9392ddb29c205badf0
pdbruno <pdbruno@gmail.com>
2023-09-20 17:43:53 -0300

    implemento ej 5 y escribo 1 de 5 tests

--- MagicFuzzer/src/magic_fuzzer.py
+++ MagicFuzzer/src/magic_fuzzer.py
@@ -2,12 +2,19 @@ from typing import List, Set
 from fuzzingbook.Coverage import Location
 from fuzzingbook.MutationFuzzer import FunctionCoverageRunner
 import random
-from src.roulette_input_selector import RouletteInputSelector 
-from src.mutation_utils import insert_random_character, delete_random_character, change_random_character
+from src.roulette_input_selector import RouletteInputSelector
+from src.mutation_utils import (
+    insert_random_character,
+    delete_random_character,
+    change_random_character,
+)
 from src.get_source_lines import get_source_lines
 
+
 class MagicFuzzer:
-    def __init__(self, initial_inputs, function_to_call, function_name_to_call = None) -> None:
+    def __init__(
+        self, initial_inputs, function_to_call, function_name_to_call=None
+    ) -> None:
         """Ejecuta inputs iniciales, almacenando la cobertura obtenida"""
         self.source_lines = get_source_lines(function_to_call)
         self.method_name = function_name_to_call
@@ -20,25 +27,25 @@ class MagicFuzzer:
             self.run_input(input)
 
     def run_input(self, input):
-        result, outcome = self.runner.run(input)
-
+        self.runner.run(input)
         locations = self.runner.coverage()
-
         self.roulette_input_selector.add_new_execution(input, locations)
 
         for location in locations:
             nombreFuncion, numeroDeLinea = location
-            if nombreFuncion not in self.lines_covered or numeroDeLinea not in self.lines_covered[nombreFuncion]:
+            if (
+                nombreFuncion not in self.lines_covered
+                or numeroDeLinea not in self.lines_covered[nombreFuncion]
+            ):
                 if input not in self.contributing_inputs:
                     self.contributing_inputs.append(input)
-                
+
                 if nombreFuncion not in self.lines_covered:
                     self.lines_covered[nombreFuncion] = [numeroDeLinea]
                 elif numeroDeLinea not in self.lines_covered[nombreFuncion]:
                     self.lines_covered[nombreFuncion].append(numeroDeLinea)
 
             self.covered_locations.add(location)
-                
 
     def get_contributing_inputs(self) -> List[str]:
         """Retorna la lista de los inputs que aumentaron la cobertura en el orden que fueron ejecutados"""
@@ -46,40 +53,57 @@ class MagicFuzzer:
 
     def get_covered_locations(self) -> Set[Location]:
         """Retorna el conjunto de locaciones cubiertas de todas las ejecuciones"""
-        return self.covered_locations.difference([('run_function', 410), ('trace', 378), ('coverage', 382)])
+        locations = self.covered_locations.difference(
+            [("run_function", 410), ("trace", 378), ("coverage", 382)]
+        )
+        return (
+            locations
+            if not self.method_name
+            else set(
+                (f_name, f_line)
+                for f_name, f_line in locations
+                if f_name == self.method_name
+            )
+        )
 
     def mutate(self, s: str) -> str:
         """Aplica al azar alguna de las tres operaciones de mutacion definidas en el archivo mutation_utils.py"""
-        return random.choice([insert_random_character, delete_random_character, change_random_character])(s)
+        return random.choice(
+            [insert_random_character, delete_random_character, change_random_character]
+        )(s)
 
     def fuzz(self):
         """
         Elije aleatoriamente un input s usando seleccion de ruleta sobre e(s),
         muta el input s utilizando la función mutate(s), y ejecuta el s mutado
         """
-        
+
         input_seleccionado = self.roulette_input_selector.select()
         input_mutado = self.mutate(input_seleccionado)
         self.run_input(input_mutado)
 
-
-    def run_until_covered(self, n = None) -> int:
+    def run_until_covered(self, n=None) -> int:
         """
         Corre una campania del MagicFuzzer hasta cubrir todas las lineas del programa.
         Retorna la cantidad de iteraciones realizadas.
         """
         iteraciones = 0
-        covered = []
-        while len(covered) != self.source_lines - 1:
-            iteraciones+=1
+        while len(self.get_covered_locations()) != self.source_lines - 1:
+            iteraciones += 1
             self.fuzz()
-            covered = [f_line for f_name, f_line in self.covered_locations if f_name == self.method_name]
         return iteraciones
 
-    def run(self, n = None) -> int:
+    def run_iterations(self, n=None) -> int:
         """
         Corre una campaña del MagicFuzzer.
         La campaña debe ser ejecutada por n iteraciones (si n no es None), o hasta cubrir todas las líneas del programa.
         Retorna la cantidad de iteraciones realizadas.
         """
-        pass
+        if not n:
+            return self.run_until_covered()
+        else:
+            iteraciones = 0
+            while iteraciones < n and len(self.get_covered_locations()) != self.source_lines - 1:
+                iteraciones += 1
+                self.fuzz()
+            return iteraciones
--- MagicFuzzer/test/test_ejercicio_5.py
+++ MagicFuzzer/test/test_ejercicio_5.py
@@ -1,9 +1,26 @@
 #!./venv/bin/python
 import unittest
+import random
+from src import my_parser
+from src import magic_fuzzer
 
+""" 
+Ejecutar 5 campañas de fuzzing usando n = 5000 sobre la función my_parser del archivo
+my_parser.py usando como input inicial   (i.e. caracter en blanco), function_name_to_call el
+parámetro vacío. Modique el archivo test_ejercicio_5.py para dejar constancia de los resultados
+obtenidos. El test debe indicar la cantidad máxima de líneas cubiertas del programa en cada campaña.
+Utilice una semilla (distinta para cada campaña) en los tests para que los resultados sean determinísticos. Nota: en algunas ocasiones se ha observado que la función my_parser puede llegar a comportarse
+de manera no determinística, incluso utilizando una semilla para el random. Si ese fuera el caso, utilice
+el script run_tests.sh para correr los tests de este ejercicio. Adenás considere implementar cada campaña de fuzzing en un test distinto para evitar que la ejecución de una campaña afecte a la siguiente
+ """
 
 class TestEjercicio5(unittest.TestCase):
 
-    def test(self):
-        # TOOD: COMPLETAR
-        self.assertEqual(True, True)
+    def test1(self):
+        random.seed(1)
+        f = magic_fuzzer.MagicFuzzer(' ', my_parser.my_parser)
+        f.run_iterations(5000)
+        cantidad_maxima_lineas_cubiertas = len(f.get_covered_locations())
+        self.assertEqual(cantidad_maxima_lineas_cubiertas, 480)
+
+#4 veces mas
