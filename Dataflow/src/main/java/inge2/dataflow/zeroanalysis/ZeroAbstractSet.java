cf7b3c106e6e144f3a1c03615f0c429b1a870aa1
Erik <erik.ernst@muttdata.ai>
2023-10-23 19:56:09 -0300

    taller 7

---
+++ Dataflow/src/main/java/inge2/dataflow/zeroanalysis/ZeroAbstractSet.java
@@ -0,0 +1,116 @@
+package inge2.dataflow.zeroanalysis;
+
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * This class represents a mapping of ZeroAbstractValues (BOTTOM, NZ, Z or MZ) to variable names.
+ */
+public class ZeroAbstractSet {
+
+    /**
+     * This map contains the mapping of variable names to ZeroAbstractValues.
+     */
+    private final HashMap<String, ZeroAbstractValue> map;
+
+    public ZeroAbstractSet() {
+        this.map = new HashMap<>();
+    }
+
+    /**
+     * This method checks if the given variable has a value in the set.
+     * @param variable the variable name.
+     * @return true if the variable has a value, false otherwise.
+     */
+    public Boolean hasValue(String variable) {
+        return this.map.containsKey(variable);
+    }
+
+    /**
+     * This method returns the value of the given variable.
+     * @param variable the variable name.
+     * @return the value of the variable.
+     */
+    public ZeroAbstractValue getValue(String variable) {
+        return this.map.get(variable);
+    }
+
+    /**
+     * This method sets the value of the given variable.
+     * @param variable the variable name.
+     * @param value the value of the variable.
+     */
+    public void setValue(String variable, ZeroAbstractValue value) {
+        if (value != null) {
+            this.map.put(variable, value);
+        }
+    }
+
+    /**
+     * This method returns the defined variables in this mapping.
+     * @return the defined variables in this mapping.
+     */
+    public Set<String> getDefinedVariables() {
+        return this.map.keySet();
+    }
+
+    /**
+     * This method returns the union of this set with another set.
+     * @param another the other set.
+     * @return the union of this set with another set.
+     */
+    public ZeroAbstractSet union(ZeroAbstractSet another) {
+        Set<String> all_variables = this.getDefinedVariables();
+        all_variables.addAll(another.getDefinedVariables());
+        ZeroAbstractSet res = new ZeroAbstractSet();
+        for (String variable_name: all_variables) {
+            ZeroAbstractValue this_value = this.getValue(variable_name);
+            ZeroAbstractValue another_value = another.getValue(variable_name);
+            ZeroAbstractValue new_value;
+
+            if (this_value == null) {
+                new_value = another_value;
+            } else if (another_value == null) {
+                new_value = this_value;
+            } else {
+                new_value = this_value.merge(another_value);
+            }
+            res.setValue(variable_name, new_value);
+        }
+        return res;
+    }
+
+    /**
+     * Clears the set.
+     */
+    public void clear() {
+        this.map.clear();
+    }
+
+    /**
+     * Copies the values of another set into this set.
+     * @param another the other set.
+     */
+    public void putAll(ZeroAbstractSet another) {
+        this.map.putAll(another.map);
+    }
+
+    @Override
+    public String toString() {
+        return "ZeroAbstractSet{" + this.map + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ZeroAbstractSet)) return false;
+        ZeroAbstractSet that = (ZeroAbstractSet) o;
+        return Objects.equals(this.map, that.map);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.map);
+    }
+}
---
+++ Dataflow/src/main/java/inge2/dataflow/zeroanalysis/ZeroAbstractValue.java
@@ -0,0 +1,125 @@
+package inge2.dataflow.zeroanalysis;
+
+/**
+ * This enum represents the possible values of the zero analysis for a variable.
+ */
+public enum ZeroAbstractValue {
+
+    /**
+     * We don't have information about the variable.
+     */
+    BOTTOM("bottom"),
+
+    /**
+     * The variable is not zero.
+     */
+    NOT_ZERO("not-zero"),
+
+    /**
+     * The variable is zero.
+     */
+    ZERO("zero"),
+
+    /**
+     * The variable may be (or not) zero.
+     */
+    MAYBE_ZERO("maybe-zero");
+
+    /**
+     * The name of the ZeroAbstractValue.
+     */
+    private final String name;
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    ZeroAbstractValue(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the result of the addition between this ZeroAbstractValue and another.
+     * @param another the other ZeroAbstractValue.
+     * @return the result of the addition.
+     */
+    public ZeroAbstractValue add(ZeroAbstractValue another) {
+        
+        if (this == ZeroAbstractValue.BOTTOM || another == ZeroAbstractValue.BOTTOM){
+            return  ZeroAbstractValue.BOTTOM;
+        } else if (this == ZeroAbstractValue.MAYBE_ZERO || another == ZeroAbstractValue.MAYBE_ZERO) {
+            return ZeroAbstractValue.MAYBE_ZERO;
+        } else if (this == ZeroAbstractValue.ZERO) {
+            return another;
+        } else {
+            if (another == ZeroAbstractValue.ZERO){
+                return ZeroAbstractValue.NOT_ZERO;
+            } else {
+                return ZeroAbstractValue.MAYBE_ZERO;
+            }
+        }
+    }
+
+    /**
+     * Returns the result of the division between this ZeroAbstractValue and another.
+     * @param another the other ZeroAbstractValue.
+     * @return the result of the division.
+     */
+    public ZeroAbstractValue divideBy(ZeroAbstractValue another) {
+        if (this == ZeroAbstractValue.BOTTOM || another == ZeroAbstractValue.BOTTOM || another == ZeroAbstractValue.ZERO){
+            return  ZeroAbstractValue.BOTTOM;
+        } else if (this == ZeroAbstractValue.ZERO && another == ZeroAbstractValue.NOT_ZERO) {
+            return ZeroAbstractValue.ZERO;
+        } else if (this == ZeroAbstractValue.NOT_ZERO && another == ZeroAbstractValue.NOT_ZERO) {
+            return ZeroAbstractValue.NOT_ZERO;
+        } else if (this == ZeroAbstractValue.MAYBE_ZERO && another == ZeroAbstractValue.NOT_ZERO) {
+            return ZeroAbstractValue.NOT_ZERO;
+        } else { // if (another == ZeroAbstractValue.MAYBE_ZERO)
+            return ZeroAbstractValue.MAYBE_ZERO;
+        }
+    }
+
+    /**
+     * Returns the result of the multiplication between this ZeroAbstractValue and another.
+     * @param another the other ZeroAbstractValue.
+     * @return the result of the multiplication.
+     */
+    public ZeroAbstractValue multiplyBy(ZeroAbstractValue another) {
+        if (this == ZeroAbstractValue.BOTTOM || another == ZeroAbstractValue.BOTTOM){
+            return  ZeroAbstractValue.BOTTOM;
+        } else if (this == ZeroAbstractValue.MAYBE_ZERO || another == ZeroAbstractValue.MAYBE_ZERO) {
+            return ZeroAbstractValue.MAYBE_ZERO;
+        } else if (this == ZeroAbstractValue.ZERO || another == ZeroAbstractValue.ZERO) {
+            return ZeroAbstractValue.ZERO;
+        } else {
+            return ZeroAbstractValue.NOT_ZERO;
+        }
+    }
+
+    /**
+     * Returns the result of the subtraction between this ZeroAbstractValue and another.
+     * @param another the other ZeroAbstractValue.
+     * @return the result of the subtraction.
+     */
+    public ZeroAbstractValue substract(ZeroAbstractValue another) {
+        return this.add(another);
+    }
+
+    /**
+     * Returns the result of the merge between this ZeroAbstractValue and another.
+     * @param another the other ZeroAbstractValue.
+     * @return the result of the merge.
+     */
+    public ZeroAbstractValue merge(ZeroAbstractValue another) {
+        if (this == ZeroAbstractValue.BOTTOM) {
+            return another;
+        } else if (another == ZeroAbstractValue.BOTTOM) {
+            return this;
+        } else if (this == another) {
+            return this;
+        } else {
+            return ZeroAbstractValue.MAYBE_ZERO;
+        }
+    }
+}
